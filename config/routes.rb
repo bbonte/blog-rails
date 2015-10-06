ad16b17fd10f0509e9ed538618e796e7c4bee82b
Bruno Bonte <bruno.bonte@gmail.com>
2015-10-06 10:38:54 -0300

    created new models, controllers, migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :posts
+  resources :posts
   devise_for :users
   get 'posts/index'
 
-  resources :posts do
-    resources :comments
-  end
+  ## resources :posts do
+  ##   resources :comments
+  ## end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
